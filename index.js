8304d223b7682c31bbf93078a5a9c7a3b062dccb
Arda Koç <32490355+ardakoc@users.noreply.github.com>
2023-06-06 16:22:12 +0300

    created app skeleton

---
+++ index.js
@@ -0,0 +1,6 @@
+const addBtn = document.getElementById("add-btn")
+const inputEl = document.getElementById("input-el")
+
+addBtn.addEventListener("click", function () {
+  console.log(inputEl.value)
+})
