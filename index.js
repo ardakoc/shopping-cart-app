ae76aa3da8a3cf92cb5b7cc2013384ddca3f481a
Arda Koç <ardaskoc@gmail.com>
2023-06-08 15:19:35 +0300

    improved functionality and fixed some bugs on click event.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://shopping-cart-16da0-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,7 +7,7 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const db = getDatabase(app)
-const shoppingList = ref(db, "shoppingList")
+const shoppingListDb = ref(db, "shoppingList")
 
 const addBtn = document.getElementById("add-btn")
 const inputEl = document.getElementById("input-el")
@@ -16,7 +16,28 @@ const shoppingListEl = document.getElementById("shopping-list")
 addBtn.addEventListener("click", function () {
   let input = inputEl.value
   
-  push(shoppingList, input)
-  shoppingListEl.innerHTML += `<li>${input}</li>`
-  inputEl.value = ""
+  push(shoppingListDb, input)
+  clearInput()
 })
+
+onValue(shoppingListDb, function(snapshot) {
+  let shoppingListArray = Object.values(snapshot.val())
+
+  clearShoppingList()
+  
+  for (let i=0; i<shoppingListArray.length; i++) {
+    appendItemToList(shoppingListArray[i])
+  }
+})
+
+function clearInput() {
+  inputEl.value = ""
+}
+
+function clearShoppingList() {
+  shoppingListEl.innerHTML = ""
+}
+
+function appendItemToList(item) {
+  shoppingListEl.innerHTML += `<li>${item}</li>`
+}
