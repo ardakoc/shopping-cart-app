1fc9880eac8b65d0164ccfddd5ac5207b221711e
Arda Koç <ardaskoc@gmail.com>
2023-06-09 10:58:36 +0300

    added an event listener for deletion of clicked item.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://shopping-cart-16da0-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -29,7 +29,7 @@ onValue(shoppingListDb, function(snapshot) {
     let currentItem = shoppingListArray[i]
     let currentItemID = currentItem[0]
     let currentItemValue = currentItem[1]
-    appendItemToList(currentItemValue)
+    appendItemToList(currentItem)
   }
 })
 
@@ -42,5 +42,12 @@ function clearShoppingList() {
 }
 
 function appendItemToList(item) {
-  shoppingListEl.innerHTML += `<li>${item}</li>`
+  let itemID = item[0]
+  let itemValue = item[1]
+  let newElement = document.createElement("li")
+  newElement.textContent = itemValue
+  newElement.addEventListener("click", function() {
+    remove(ref(db, `shoppingList/${itemID}`))
+  })
+  shoppingListEl.append(newElement)
 }
